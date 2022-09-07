6fc304359b5ff1c6383e2021e40afcb2edc80301
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2022-09-07 10:03:35 +0100

    added refactor solving problem 7

--- models/games-model.js
+++ models/games-model.js
@@ -7,16 +7,39 @@ exports.readCategories = () => {
 };
 
 exports.readReviewObject = (id) => {
+  let votes = 0;
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [id])
+    .query(
+      `SELECT reviews.review_id, review_body, title, designer, review_img_url, reviews.votes, category, owner, reviews.created_at FROM reviews 
+    LEFT JOIN comments 
+    ON comments.review_id=reviews.review_id 
+    WHERE reviews.review_id=$1`,
+      [id]
+    )
     .then(({ rows }) => {
       if (rows.length > 0) {
-        return rows[0];
+        for (num of rows) {
+          if (rows.length === 1) {
+            return rows[0];
+          } else if (rows.length > 1) {
+            num.comment_count = rows.length;
+          }
+          return rows[0];
+        }
       } else {
         return Promise.reject({ status: 404, msg: `Review id doesn't exist` });
       }
     });
 };
+//     .query(`SELECT * FROM reviews WHERE review_id = $1;`, [id])
+//     .then(({ rows }) => {
+//       if (rows.length > 0) {
+//         return rows[0];
+//       } else {
+//         return Promise.reject({ status: 404, msg: `Review id doesn't exist` });
+//       }
+//     });
+// };
 
 exports.readUsers = () => {
   return db.query(`SELECT * FROM users;`).then((result) => {
