01afdcdaa1b768b1e07c0af25363d5f2ed3f77d3
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2022-09-08 14:42:36 +0100

    heroku connection file fixed

--- db/connection.js
+++ db/connection.js
@@ -9,4 +9,14 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+module.exports = new Pool(config);
