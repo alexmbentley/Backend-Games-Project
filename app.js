1f084da648ca21d9e5cd8f287811ae07cad5d062
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2022-10-12 19:49:39 +0100

    added /api endpoint

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const {
   getComments,
   addComment,
   deleteCommentByID,
+  getAPI,
 } = require('./contollers/games-controller');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api', getAPI);
 app.get('/api/categories', getCategories);
 app.get('/api/reviews', getReviews);
 app.get('/api/reviews/:review_id', getReviewObject);
@@ -25,7 +27,9 @@ app.post('/api/reviews/:review_id/comments', addComment);
 app.delete('/api/comments/:comment_id', deleteCommentByID);
 
 app.all('/*', (req, res, next) => {
-  res.status(404).send({ msg: 'Path not found' });
+  res
+    .status(404)
+    .send({ msg: 'Path not found, please use /api to see all endpoints' });
 });
 
 app.use((err, req, res, next) => {
--- contollers/games-controller.js
+++ contollers/games-controller.js
@@ -95,3 +95,10 @@ exports.deleteCommentByID = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getAPI = (req, res, next) => {
+  return fs.readFile(`${__dirname}/../endpoints.json`, 'utf-8').then((data) => {
+    data = JSON.parse(data);
+    res.status(200).send({ data });
+  });
+};
